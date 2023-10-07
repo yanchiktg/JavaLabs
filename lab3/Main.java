b673cd42ff5575063161f2f92d296116ba7ec439
yanchi <85614248+yanchiktg@users.noreply.github.com>
2023-10-07 19:49:17 +0300

    Main.java

--- lab3/Main.java
+++ lab3/Main.java
@@ -14,45 +14,9 @@ public class Main {
         Man p = new Passenger();
         p.aboutMe();
 
-//        Man m = new Employee();
-//        m.aboutMe();
-//        Stations[] values = Stations.values();
-//        for (Stations c: values){
-//            System.out.println(c.ordinal() + " " + c.name());
-//        }
-//
-//        Stations stations = Stations.valueOf(new Scanner(System.in).nextLine());
-//        stations.output();
-
         Ticket t = new Ticket();
         t.fill(scanner);
         System.out.println(t.toString());
-
-
-//        System.out.println("input wagonType: ");
-//        WagonType[] values = WagonType.values();
-//        for (WagonType value : values) {
-//            System.out.println(value.ordinal() + " " + value.name());
-//        }
-//
-//        WagonType userWagonType = null;
-//        String inputWagonType = scanner.next();
-//
-//        switch (inputWagonType){
-//            case "0" :
-//            case "PASSENGER" :
-//                userWagonType = WagonType.PASSENGER; break;
-//            case "1":
-//            case "HIGHSPEED":
-//                userWagonType = WagonType.HIGHSPEED; break;
-//            case "2":
-//            case "TOURIST":
-//                userWagonType = WagonType.TOURIST; break;
-//            case "3":
-//            case "EXPRESS":
-//                userWagonType = WagonType.EXPRESS; break;
-//        }
-//
-//        System.out.println("User wagonType is " + userWagonType.name());
+        
     }
 }
