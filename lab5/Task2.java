083f34ccde88d075d687f02f2b883e01915ea98f
yanchi <85614248+yanchiktg@users.noreply.github.com>
2023-10-10 23:50:12 +0300

    Тask2.java

--- lab5/Task2.java
+++ lab5/Task2.java
@@ -72,50 +72,5 @@ public class Task2 {
                 System.out.println(e.getMessage() + " try again");
             }
         }
-
-
-//        if(a > b || a == b)
-//            System.out.print("Invalid values!");
-//        else{
-//            System.out.println("line segment = [" + a + ";" + b + "]");
-//            System.out.print("Enter x = ");
-//            double x = scanner.nextDouble();
-//
-//            if(x < a){
-//                if(x > 0){
-//                    res = Math.cos(x + Math.log(x)) + Math.pow(Math.E, x);
-//                    System.out.print("Result = " + res);
-//                }
-//                else{
-//                    System.out.print("Error log() < 0!");
-//                }
-//            }
-//
-//            else if(x >= a && x <= b){
-//                res = Math.pow(x, 3) + Math.pow(x, 2) - 15;
-//                System.out.print("Result = " + res);
-//            }
-//
-//            else{
-//                if((Math.tan(Math.PI * x)) != 0){
-//                    res = (x + 5.0)/(Math.tan(Math.PI * x));
-//                    System.out.print("Result = " + res);
-//                }
-//                else{
-//                    System.out.print("Error tg = 0!");
-//                }
-//            }
-//
-//        }
     }
-
-//    public static double func(double a, double b, double x) throws ArithmeticException{
-//        if(x < a){
-//            if(x <= 0)
-//                return Math.cos(x + Math.log(x)) + Math.pow(Math.E, x);
-//            else{
-//                System.out.print("Error log() < 0!");
-//            }
-//        }
-//    }
 }
